62f39c56cb2ed4880319d5089781e5ac5ec13631
gksankar7 <57542387+gksankar7@users.noreply.github.com>
2023-07-10 23:35:13 +0530

    initial commit

---
+++ main.tf
@@ -0,0 +1,23 @@
+resource "google_storage_bucket" "auto-expire" {
+  name          = "inbound-centaur-392218-bucket"
+  location      = "US"
+  force_destroy = true
+
+  lifecycle_rule {
+    condition {
+      age = 3
+    }
+    action {
+      type = "Delete"
+    }
+  }
+
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
+}
--- providers.tf
+++ providers.tf
@@ -1,1 +1,15 @@
+terraform {
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.72.1"
+    }
+  }
+}
+
+provider "google" {
+  project = "inbound-centaur-392218"
+  region  = "us-central1"
+  zone    = "us-central1-c"
+}
 
